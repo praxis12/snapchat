ae7ddfc2d147934763f4a7815ca674e0645890a5
Josh Arntz <jarntz12@gmail.com>
2017-04-26 16:29:38 -0500

    Added Firebase and renamed SignInViewController

---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Snapchat Clone' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Snapchat Clone
+pod 'Firebase/Core'
+
+  target 'Snapchat CloneTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'Snapchat CloneUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ Snapchat Clone/SignInViewController.swift
@@ -0,0 +1,26 @@
+//
+//  SignInViewController.swift
+//  Snapchat Clone
+//
+//  Created by Josh Arntz on 4/26/17.
+//  Copyright © 2017 Mr. Bear. All rights reserved.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    @IBAction func turnUpTapped(_ sender: Any) {
+    }
+
+   
+
+
+}
+
